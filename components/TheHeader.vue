363499ce2c3e3c6b3f71ed5681d846592a328e32
Timothée Thiry <timothee.thiry@gmail.com>
2021-09-23 16:48:12 +0200

    style: centering of about and header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,37 +1,33 @@
 <template>
-    <div class="flex-1 flex flex-col">
-        <nav class="px-4 flex justify-between bg-white h-16 border-b-2">
+    <div>
+        <nav class="px-4 flex flex-wrap justify-center sm:justify-between bg-white h-16 border-b-2">
 
             <!-- top bar left -->
             <ul class="flex items-center">
                 <!-- add button -->
-                <li class="h-6 mr-6">
+                <li class="">
+                    <h1 class="text-gray-700">Backpack with me!</h1>
+                </li>
+            </ul>
+
+            <ul class="flex items-center sm:mx-auto transform sm:-translate-x-11">
+                <!-- add button -->
+                <li class="h-6 mx-6 sm:ml-0 sm:mr-6">
                     <nuxt-link class="" to="/">Accueil</nuxt-link>
                 </li>
-                <li class="h-6 mr-6">
+                <li class="h-6 mr-6 sm:m-0">
                     <nuxt-link class="" to="/about">Who is this guy ?</nuxt-link>
                 </li>
             </ul>
 
-            <ul class="flex items-center">
-                <!-- add button -->
-                <li>
-                    <h1 class="pl-8 lg:pl-0 text-gray-700">Backpack with me!</h1>
-                </li>
-            </ul>
-
             <!-- to bar right  -->
-            <ul class="flex items-center">
-
-                <li class="pr-6">
-                </li>
+            <ul class="hidden sm:flex items-center ">
                 <li class="h-10 w-10">
                     <img
                         class="h-full w-full rounded-full mx-auto"
                         src="https://images.unsplash.com/19/nomad.JPG?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80"
                         alt="profile man" />
                 </li>
-
             </ul>
         </nav>
     </div>
--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,11 @@
 <template>
     <div>
-        <div v-for="(item, index) in person.items" :key="index">
-            <p>{{ item.fields.name }}</p>
-            <div class="w-40 h-40 object-contain">
+        <div class="text-center m-4" v-for="(item, index) in person.items" :key="index">
+            <div class="mx-auto mb-4 w-40 h-40 object-contain">
                 <img id="photo" :src="photo[index].fields.file.url">   
             </div>
+            <h2>{{ item.fields.name }}</h2>
+            <h1>{{ item.fields.title }} </h1>
             <p>{{ item.fields.shortBio }} </p>
         </div>
     </div>
